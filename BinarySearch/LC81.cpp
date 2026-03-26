5c7992cfbac8ff523d4098d07f4d2d242bda2b6e
Shubham Vishwakarma <sv8112004@gmail.com>
2026-03-26 23:03:21 +0530

    Search in Rotated Sorted Array-II

---
+++ BinarySearch/LC81.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool searchInRotatedArray(vector<int>& nums, int target) {
+    int low = 0;
+    int high = nums.size() - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (nums[mid] == target) return true;
+
+        // Handle duplicates
+        if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+            low++;
+            high--;
+        }
+        // Left half sorted
+        else if (nums[low] <= nums[mid]) {
+            if (nums[low] <= target && target < nums[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        // Right half sorted
+        else {
+            if (nums[mid] < target && target <= nums[high]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+    }
+
+    return false;
+}
+
+int main() {
+    vector<int> nums = {2, 5, 6, 0, 0, 1, 2};
+    int target = 0;
+
+    if (searchInRotatedArray(nums, target)) {
+        cout << "Target found" << endl;
+    } else {
+        cout << "Target not found" << endl;
+    }
+
+    return 0;
+}
